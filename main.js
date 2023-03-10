583b804f1376959f3ea59e6893bf87119b89ac72
cad6764418097000 <74503191+cad6764418097000@users.noreply.github.com>
2023-03-10 14:53:58 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,4 +7,15 @@ var boardDOM = $("#board")
 
 
 
-movePiece(wKnight, 100, 100);
+//movePiece(wKnight, 100, 100);
+
+
+
+$('#board').click(function(e){
+
+
+  const indexx = Math.ceil(e.offsetX / 62);
+  const indexy = Math.ceil(e.offsetY / 62);
+
+  board.highlightSquare(indexx, indexy, "#FFFF00");
+});
